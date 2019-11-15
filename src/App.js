5704ffd070df5c4766f846f5dbacecde2e647f3c
Liam Mitchell <git@liam.geek.nz>
2019-11-15 19:26:16 +0100

    Auto deploy and format code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div>
-      <h1 style={{textAlign: 'center'}}>SC2 Things</h1>
+      <h1 style={{ textAlign: "center" }}>SC2 Things</h1>
     </div>
   );
 }

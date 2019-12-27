ec2c05b23cccf1638d777d3a00642dca74641ec3
Liam Mitchell <git@liam.geek.nz>
2019-12-27 17:31:10 +0100

    Nicer not found icon

--- src/Icon.js
+++ src/Icon.js
@@ -2,19 +2,36 @@ import React from "react";
 import icons from "./icons.json";
 
 function Icon({ id, style }) {
-  // CancelMutateMorph is not a bad "missing image" icon.
-  const [x, y, width, height] = icons[id] || icons.CancelMutateMorph;
+  const icon = icons[id];
+  const [x, y, width, height] = icon || [-76, -76, 76, 76];
 
   return (
     <div
+      title={id}
       style={{
         ...style,
-        backgroundImage: "url(/icons.jpg)",
-        backgroundPosition: `-${x}px -${y}px`,
+        backgroundColor: "black",
+        backgroundImage: icon && "url(/icons.jpg)",
+        backgroundPosition: icon && `${-x}px ${-y}px`,
         width,
         height
       }}
-    />
+    >
+      {!icon && (
+        <div
+          style={{
+            fontSize: 10,
+            padding: 8,
+            color: "white",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap"
+          }}
+        >
+          {id}
+        </div>
+      )}
+    </div>
   );
 }
 
